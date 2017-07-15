b26151e1ee501a68f506de98d21c4da4f9e10a1c
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-07-15 15:30:52 -0500

    First dengrogram plot

--- compressibility.R
+++ compressibility.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(tidygraph)
 library(ggraph)
+library(igraph)
 library(lme4)
 library(broom)
 
@@ -14,11 +15,34 @@ models <- data_frame(
       "logpopall + numUniqueChars +                  origSize + (logpopall|pair)",
       "logpopall + numUniqueChars + numUniqueWords +            (logpopall|pair)"
     )),
+    difference = c("", "— numUniqueChars", "— numUniqueWords", "— origSize"),
     model = sapply(formula, function(f) lmer(f, data = bibles))
-  )
+  ) %>%
+  mutate(n = 1:n()) %>%
+  select(n, everything()) %>%
+  mutate(formula_labeled = ifelse(n == 1, formula, difference))
 
-models %>%
-  rowwise() %>%
+mod_summaries <- models %>%
+  group_by(formula) %>%
   do(
-    tidy(.$model, effects = "fixed")
-  )
+    tidy(.$model[[1]], effects = "fixed")
+  ) %>%
+  ungroup() %>%
+  filter(term == "logpopall")
+
+models <- left_join(models, mod_summaries)
+
+edges <- data_frame(
+  from = 1,
+  to = c(2, 3, 4),
+  difference = c("- numUniqueChars", "- numUniqueWords", "- origSize")
+)
+
+graph <- graph_from_data_frame(edges, vertices = models)
+
+ggraph(graph, layout = "dendrogram") +
+  geom_edge_diagonal() +
+  geom_node_label(aes(label = formula_labeled, color = statistic), size = 2) +
+  theme_graph() +
+  coord_cartesian(xlim = c(0, 4), ylim = c(0, 1)) +
+  scale_color_gradient2()
