8c744862a36ce9b83e81fc4214004d28f6b2c95d
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-07-17 16:04:42 -0500

    Create formula permutations

--- R/formulas.R
+++ R/formulas.R
@@ -1,3 +1,11 @@
 generate_formulas <- function(base_formula, covariates) {
-  base_formula
+  n_covariates <- length(covariates)
+  covariate_ids <- unlist(
+    lapply(1:n_covariates, function(m) combn(1:n_covariates, m, simplify = FALSE)),
+    recursive = FALSE
+  )
+  formulas <- purrr::map(covariate_ids, function(ids) {
+    paste(base_formula, paste(covariates[ids], collapse = " + "), sep = " + ")
+  })
+  c(base_formula, unlist(formulas))
 }
--- tests/testthat/test_formulas.R
+++ tests/testthat/test_formulas.R
@@ -1,6 +1,6 @@
 context("Generating formulas")
 
 test_that("basic formulas can be generated", {
-  formulas <- generate_formulas(y ~ x, c("a", "b"))
-  expect_equal(formulas, c(y ~ x, y ~ x + a, y ~ x + b, y ~ x + a + b))
+  formulas <- generate_formulas("y ~ x", c("a", "b"))
+  expect_equal(formulas, c("y ~ x", "y ~ x + a", "y ~ x + b", "y ~ x + a + b"))
 })
