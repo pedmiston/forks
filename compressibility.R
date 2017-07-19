c222ad5761c073d969ebec6298c32d9b696227a7
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-07-19 15:52:24 -0500

    Plot compressibility models with 3 vars
    Doesn't work because filtering based on n_different
    doesn't do what it's supposed to.

--- compressibility.R
+++ compressibility.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(tidygraph)
+library(igraph)
 library(ggraph)
 library(lme4)
 library(broom)
@@ -13,9 +14,18 @@ formulas <- generate_formulas("compression ~ logpopall + (logpopall|pair)",
                               c("numUniqueChars", "numUniqueWords", "origSize"))
 
 models <- data_frame(
-  model_id = 1:length(formulas),
   formula = formulas,
   model = map(formulas, lmer, data = bibles)
 )
 
-# edges <- get_deviations(formulas)
+edges <- get_deviations(formulas)
+
+graph <- graph_from_data_frame(edges, vertices = models)
+
+ggraph(graph, layout = "kk") +
+  geom_edge_link(aes(label = difference),
+                 angle_calc = 'along',
+                 label_dodge = unit(2.5, 'mm'),
+                 arrow = arrow(length = unit(4, 'mm')), 
+                 end_cap = circle(3, 'mm')) +
+  geom_node_point()
