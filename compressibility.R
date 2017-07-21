8a06894f3bb265dc1b1f4eeb7bc42297d7d7f802
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-07-20 19:57:15 -0500

    Extend to 4-covariats

--- compressibility.R
+++ compressibility.R
@@ -5,35 +5,45 @@ library(ggraph)
 library(lme4)
 library(broom)
 
+# Load this package, e.g. everything in the "R/" directory
 devtools::load_all(".")
 
 bibles <- read_csv("bibles.csv")
 bibles$compression <- with(bibles, gzip1/origSize)
 
+# Generate all model formulas with additions of various covariates
 formulas <- generate_formulas("compression ~ logpopall + (logpopall|pair)",
                               c("numUniqueChars", "numUniqueWords", "origSize"))
 
+# Create a data_frame of formulas and lmer model objects
 models <- data_frame(
   formula = formulas,
   model = map(formulas, lmer, data = bibles),
+  # Identify the first formula as the base model
   is_base = c(TRUE, rep(FALSE, times = length(formulas) - 1))
 )
 
+# Extract the model estimates and statistics from each model
 model_summaries <- models %>%
   rowwise() %>%
   do({
+    # Tidy the model, and add the model formula as the first column
     mod_summary <- tidy(.$mod, effects = "fixed")
     mod_summary$formula <- .$formula
     mod_summary %>% select(formula, everything())
   }) %>%
+  # Filter only model estimates for the factor of interest
   filter(term == "logpopall")
-
+# Append the model estimate columns to the models dataframe
 models <- left_join(models, model_summaries)
 
+# Create edges for all 1-variable differences between model formulas
 edges <- get_deviations(formulas)
 
+# Create an igraph::graph object from edges and nodes
 graph <- graph_from_data_frame(edges, vertices = models)
 
+# Draw the igraph::graph object with ggraph
 ggraph(graph, layout = "kk") +
   geom_edge_link(aes(label = difference),
                  angle_calc = 'along',
@@ -45,4 +55,57 @@ ggraph(graph, layout = "kk") +
                   size = 5) +
   coord_cartesian(xlim = c(-1, 1.4), ylim = c(-1.5, 1.5))
 
-ggsave("compressibility.png")
+# ----------------------
+# Extend to 4 covariates
+
+formulas <- generate_formulas("compression ~ logpopall + (logpopall|pair)",
+                              c("numUniqueChars", "numUniqueWords", "origSize", "Latitude"))
+# Create a data_frame of formulas, lmer model objects, and model statistics
+models <- data_frame(
+    formula = formulas,
+    model = map(formulas, lmer, data = bibles),
+    # Identify the first formula as the base model
+    is_base = c(TRUE, rep(FALSE, times = length(formulas) - 1))
+  ) %>%
+  # Extract the model estimates and statistics from each model
+  left_join(
+    rowwise(.) %>%
+    do({
+      # Tidy the model, and add the model formula as the first column
+      mod_summary <- tidy(.$mod, effects = "fixed")
+      mod_summary$formula <- .$formula
+      mod_summary %>% select(formula, everything())
+    }) %>%
+    # Filter only model estimates for the factor of interest
+    filter(term == "logpopall")
+  )
+
+# Create edges for all 1-variable differences between model formulas
+edges <- get_deviations(formulas)
+
+# Create an igraph::graph object from edges and nodes
+graph <- graph_from_data_frame(edges, vertices = models)
+
+# Draw the igraph::graph object with ggraph
+ggraph(graph, layout = "kk") +
+  geom_edge_link(aes(label = difference),
+                 angle_calc = 'along',
+                 label_dodge = unit(2.5, 'mm'),
+                 arrow = arrow(length = unit(4, 'mm')), 
+                 end_cap = circle(3, 'mm')) +
+  geom_node_point() +
+  geom_node_label(aes(filter = is_base, label = name), vjust = -0.5,
+                  size = 5) +
+  theme_graph()
+
+ggraph(graph, layout = "with_sugiyama") +
+  geom_edge_link(aes(label = difference),
+                 angle_calc = 'along',
+                 label_dodge = unit(2.5, 'mm'),
+                 arrow = arrow(length = unit(4, 'mm')), 
+                 end_cap = circle(3, 'mm')) +
+  geom_node_point(aes(size = statistic, )) +
+  geom_node_label(aes(filter = is_base, label = name), vjust = -0.5,
+                  size = 5) +
+  coord_cartesian(xlim = c(-0.5, 6.5), c(0.2, 6)) +
+  theme_graph()
